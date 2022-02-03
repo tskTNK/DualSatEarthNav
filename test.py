bcff75b3d7f5b369c2fccc297d94e2485075c10a
tnk <bump.bunny@gmail.com>
2022-02-03 10:43:03 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from gnsspy.io.readFile import read_obsFile
 
 stationR = read_obsFile("mtka0320.21o")
 orbitR = sp3_interp(stationR.epoch, interval=stationR.interval, poly_degree=16, sp3_product="gfz", clock_product="gfz")
-
+#
 # spp_resultR = spp(stationR, orbitR, system="G", cut_off=7.0) # return (x_coordinate, y_coordinate, z_coordinate, rec_clock)
 # print (stationR.approx_position)
 # print (spp_resultR)
@@ -23,45 +23,24 @@ orbit = sp3_interp(station.epoch, interval=station.interval, poly_degree=16, sp3
 # print (station.approx_position)
 # print (spp_result)
 
-#-----------------------------------------------------------------------------
-if station.epoch == stationR.epoch:
-    print("epochs are same")
-else:
-    raise Warning("Start epochs are different")
-# ----------------------------------------------------------------------------
-#-----------------------------------------------------------------------------
-if station.interval == stationR.interval:
-    print("intervals are same")
-else:
-    raise Warning("intervals are different")
-# ----------------------------------------------------------------------------
+# #-----------------------------------------------------------------------------
+# if station.epoch == stationR.epoch:
+#     print("epochs are same")
+# else:
+#     raise Warning("Start epochs are different")
+# # ----------------------------------------------------------------------------
+# #-----------------------------------------------------------------------------
+# if station.interval == stationR.interval:
+#     print("intervals are same")
+# else:
+#     raise Warning("intervals are different")
+# # ----------------------------------------------------------------------------
+#
+# initialDifference = [station.approx_position[0]-stationR.approx_position[0], station.approx_position[1]-stationR.approx_position[1], station.approx_position[2]-stationR.approx_position[2]]
 
-initialDifference = [station.approx_position[0]-stationR.approx_position[0], station.approx_position[1]-stationR.approx_position[1], station.approx_position[2]-stationR.approx_position[2]]
-print ("\n Difference: initial")
-print (initialDifference[0], initialDifference[1], initialDifference[2])
-print ("\n")
-
-spp_resultmdpo = mdpo(station, stationR, orbitR, system="G", cut_off=7.0)
-print ("\n MDPO estimation")
-print (spp_resultmdpo[0], spp_resultmdpo[1], spp_resultmdpo[2])
-print ("\n Difference: MDPO estimation")
-print (initialDifference[0]-spp_resultmdpo[0], initialDifference[1]-spp_resultmdpo[1], initialDifference[2]-spp_resultmdpo[2])
-print ("\n")
 
 spp_resultdpp = dpp(station, stationR, orbitR, system="G", cut_off=7.0) # return (x_coordinate, y_coordinate, z_coordinate, rec_clock)
-print ("\n single estimation: station")
-print (station.approx_position)
-print (spp_resultdpp[0],spp_resultdpp[1],spp_resultdpp[2],spp_resultdpp[3])
-print ("\n difference")
-print (station.approx_position[0]-spp_resultdpp[0],station.approx_position[1]-spp_resultdpp[1],station.approx_position[2]-spp_resultdpp[2])
-print ("\n single estimation: stationR")
-print (stationR.approx_position)
-print (spp_resultdpp[4],spp_resultdpp[5],spp_resultdpp[6],spp_resultdpp[7])
-print ("\n difference")
-print (stationR.approx_position[0]-spp_resultdpp[4],stationR.approx_position[1]-spp_resultdpp[5],stationR.approx_position[2]-spp_resultdpp[6])
-print ("\n Differential GPS with all possible satellites")
-print (spp_resultdpp[0]-spp_resultdpp[4], spp_resultdpp[1]-spp_resultdpp[5], spp_resultdpp[2]-spp_resultdpp[6])
-print ("\n Difference: Differential GPS with all possible satellites")
-print (initialDifference[0]-(spp_resultdpp[0]-spp_resultdpp[4]), initialDifference[1]-(spp_resultdpp[1]-spp_resultdpp[5]), initialDifference[2]-(spp_resultdpp[2]-spp_resultdpp[6]))
+
+# spp_resultmdpo = mdpo(station, stationR, orbitR, system="G", cut_off=7.0)
 
 # python setup.py install
